c433b00fef9b4deced406fffcc67d630f9e9d473
Artur Maciel <arturma30@gmail.com>
2024-10-19 12:04:15 -0300

    add classes User, Person, Institution. Incluido User no dbcontext

---
+++ src/Volunteia/Models/Institution.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Volunteia.Models
+{
+    public class Institution : User
+    {
+        [Required(ErrorMessage = "Obrigatório informar endereço.")]
+        [MaxLength(150)]
+        [Display(Name = "Endereço")]
+        public string Adress { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar CNPJ.")]
+        [MaxLength(18)]
+        public string CNPJ { get; set; }
+        [Required(ErrorMessage = "Informe uma descrição sobre a empresa.")]
+        [MaxLength(500)]
+        [Display(Name = "Descrição da Empresa")]
+        public string ShortBio { get; set; }
+    }
+}
---
+++ src/Volunteia/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Volunteia.Models
+{
+    [Table("User")]
+    public abstract class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar seu nome.")]
+        [MaxLength(200)]
+        [Display(Name ="Nome")]
+        public string Name { get; set; }
+        public int NumberOfActions { get; set; }
+        public ICollection<Action> ActionLog { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar seu email.")]
+        [MaxLength(200)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar seu telefone.")]
+        [MaxLength(200)]
+        [Display(Name = "Telefone")]
+        public string Phone { get; set; }
+        public bool IsRestricted { get; set; }
+        public bool IsBenefactor { get; set; }
+        public bool IsPrivate { get; set; }
+        public UserStatus UserStatus { get; set; }
+
+        public ICollection<Action> Action;
+    }
+
+    public enum UserStatus
+    {
+        Admin,
+        User
+    }
+}
