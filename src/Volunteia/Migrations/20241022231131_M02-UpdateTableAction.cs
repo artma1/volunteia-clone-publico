d8102b4a4a38f5356dee9ad9d75255dbeb6b37f5
Sharon Steicy dos Santos Vasquez Vasquez <1518243@sga.pucminas.br>
2024-10-22 20:24:07 -0300

    Crianção de Controller e View Actions

---
+++ src/Volunteia/Migrations/20241022231131_M02-UpdateTableAction.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Volunteia.Migrations
+{
+    /// <inheritdoc />
+    public partial class M02UpdateTableAction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tipo",
+                table: "Action",
+                newName: "ODS");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ODS",
+                table: "Action",
+                newName: "Tipo");
+        }
+    }
+}
