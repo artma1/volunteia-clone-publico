4157854c233dfdf605677891c34fa944977d0f5b
Sharon Steicy <166241111+sharonsteicys@users.noreply.github.com>
2024-10-27 12:40:12 -0300

    ad

---
+++ src/Volunteia/Migrations/20241027153508_CreateModelUserActions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Volunteia.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateModelUserActions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    NumberOfActions = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    IsRestricted = table.Column<bool>(type: "bit", nullable: false),
+                    IsBenefactor = table.Column<bool>(type: "bit", nullable: false),
+                    IsPrivate = table.Column<bool>(type: "bit", nullable: false),
+                    UserStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserActions",
+                columns: table => new
+                {
+                    ActionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Patrocinador = table.Column<bool>(type: "bit", nullable: false),
+                    ActionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ActionBio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ODS = table.Column<int>(type: "int", nullable: false),
+                    ActionStatus = table.Column<int>(type: "int", nullable: false),
+                    VolunteersTotal = table.Column<int>(type: "int", nullable: false),
+                    NumberOfPeopleHelped = table.Column<int>(type: "int", nullable: false),
+                    Rate = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserActions", x => x.ActionId);
+                    table.ForeignKey(
+                        name: "FK_UserActions_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActions_UserId",
+                table: "UserActions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserActions");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
